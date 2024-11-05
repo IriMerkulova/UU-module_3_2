c3556aeaef24ab0eda6706d3901699a33550c978
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2024-11-05 13:57:33 +0300

    Add files via upload
    дз способы вызова функций загрузка

---
+++ module_3_2.py
@@ -0,0 +1,18 @@
+# "Рассылка писем"
+
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    selected_email = (".com", ".ru", ".net")
+    if "@" not in sender or not sender.endswith(selected_email) or \
+            "@" not in recipient or not recipient.endswith(selected_email):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == "university.help@gmail.com":
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
